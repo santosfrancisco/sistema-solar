24f0b328f929b152a65f9472052a68533d508ca5
Francisco Santos <fwfs1985@gmail.com>
2020-09-05 23:37:50 -0300

    welcome page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import colors from '../../styles/colors';
+import SolarSystem from '../../components/backgrounds/SolarSystem'
+import {
+  Container,
+  BGWrapper,
+  Subtitle,
+  Title,
+  ContentWrapper,
+  ButtonWrapper,
+  ReadyText,
+  BeginButton,
+  ButtonText,
+} from './styles';
 
 const Welcome: React.FC = () => {
+  const navigation = useNavigation()
+
+  const handleClick = () => {
+    navigation.navigate('App')
+  }
   return (
     <Container>
-      <Text>Welcome</Text>
+      <BGWrapper>
+        <SolarSystem />
+      </BGWrapper>
+      <ContentWrapper>
+        <Subtitle>
+          Aperte o cinto
+        </Subtitle>
+        <Title>
+          Comece sua jornada pelo sistema solar.
+        </Title>
+        </ContentWrapper>
+
+        <ButtonWrapper>
+          <ReadyText>
+            Pronto para a decolagem?
+          </ReadyText>
+          <TouchableOpacity onPress={handleClick}>
+            <BeginButton colors={colors.gradients.button}>
+              <ButtonText>Começar agora</ButtonText>
+              <Feather
+                name="arrow-right"
+                size={16}
+                color={colors.brand.white}
+              />
+            </BeginButton>
+          </TouchableOpacity>
+        </ButtonWrapper>
     </Container>
   );
 };
--- src/pages/Welcome/styles.ts
+++ src/pages/Welcome/styles.ts
@@ -1,5 +1,65 @@
 import styled from 'styled-components/native';
+import colors from '../../styles/colors';
+import constants from 'expo-constants';
+import { Platform } from 'react-native';
+import typography from '../../styles/typography';
+import { LinearGradient } from 'expo-linear-gradient';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
+  padding-top: ${Platform.OS === 'android' ? constants.statusBarHeight : 0}px;
+  flex: 1;
+`;
+
+export const BGWrapper = styled.View`
+  background: ${colors.brand.background};
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export const ContentWrapper = styled.View`
+  align-items: center;
+  justify-content: center;
+  padding: 34px 20px 0;
+  flex: 1;
+`;
+
+export const Subtitle = styled.Text`
+  font: ${typography.paragraph};
+  color: ${colors.brand.white};
+  opacity: .65;
+`;
+
+export const Title = styled.Text`
+  font: ${typography["home-title"]};
+  color: ${colors.brand.white};
+  text-align: center;
+`;
+
+export const ButtonWrapper = styled.View`
+  align-items: center;
 
 `;
+
+export const ReadyText = styled.Text`
+  color: ${colors.brand.white};
+  font: ${typography.placeholder};
+  margin-bottom: 16px;
+`
+
+export const BeginButton = styled(LinearGradient)`
+  width: 206px;
+  height: 51px;
+  border-radius: 8px;
+  background: ${colors.gradients.button[1]};
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 30%;
+`
+
+export const ButtonText = styled.Text`
+  font: ${typography["button-title"]};
+  color: ${colors.brand.white};
+  margin-right: 16px;
+`
